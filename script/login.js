ffa0d0a64d2a525c15805e122ffbc558a23d980f
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-11 15:13:05 +0200

    admin

--- script/login.js
+++ script/login.js
@@ -1,20 +1,21 @@
-let username = document.querySelectorAll('input')[0];
-let password = document.querySelectorAll('input')[1];
-let login = document.querySelector('button');
+let user = document.querySelectorAll('input')[0];
+let pass = document.querySelectorAll('input')[1];
+let login = document.getElementsByClassName('login')[0]
 
 function Login(username, password){
     this.username = username;
     this.password = password;
 }
 
-const admin1 = new Login['Admin1', 'Admin101']
-const admin2 = new Login['Admin2', 'Admin102']
-const Admin = [admin1, admin2]
+// const admin1 = new Login['Admin1', 'Admin101']
+// const admin2 = new Login['Admin2', 'Admin102']
+// const Admin = [admin1, admin2]
 
-Admin.forEach(item => {
-    login.addEventListener('click', ()=>{
-        if(username == `${item.username}` && password == `${item.password}` ){
-            window.location.replace = "https://www.w3schools.com";
-        }
-    })
-});
+
+// Admin.forEach(item => {
+//     login.addEventListener('click', ()=>{
+//         if(user == `${item.username}` && pass == `${item.password}` ){
+//             window.location.assign("/admin.html");
+//         }
+//     })
+// });
