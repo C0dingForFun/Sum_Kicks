1ad33e866439ba0587a91242cb6499cb353322d8
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-12 16:20:36 +0200

    products

--- script/products.js
+++ script/products.js
@@ -1,7 +1,8 @@
-function myKicks(id, name, category, image,description, quantity, price){
+function myKicks(id, brand, name, image, description, quantity, price){
     this.id = id;
     this.name = name;
-    this.category = category;
+    this.name = name;
+    this.brand = brand;
     this.image = image;
     this.description = description
     this.quantity = quantity;
@@ -9,13 +10,13 @@ function myKicks(id, name, category, image,description, quantity, price){
     
 }
 
-const item1 = new myKicks(1, '', '', 
-    'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(Black).png'
+const item1 = new myKicks(1, 'Nike', 'Air Force 1', 
+    'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(White).png'
     ,'A mash-up of Pegasus past, the Nike P-6000 delivers an early-2000s look with breathable mesh and synthetic overlays. Vibrant colours give it a modern look while its sporty aesthetic features foam cushioning for all-day comfort.', 
     1,29.99);
-const item2 = new myKicks(2, '', 'Clothing', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(Black).png',
+const item2 = new myKicks(2, 'Nike', 'Clothing', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/KD17(Sunrise).png',
      'Slim-fit denim jeans for men, made with premium cotton',1, 49.99);
-const item3 = new myKicks(3, '', 'Electronics', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(Black).png', 
+const item3 = new myKicks(3, 'Nike', 'Electronics', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(Black).png', 
     'Noise-canceling wireless beats headphones with up to 20 hours of battery life',1, 129.99);
 const item4 = new myKicks(4, '', 'Books', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(Black).png',
      'The latest bestselling novel by a renowned author',1, 14.99);
@@ -23,22 +24,18 @@ const item5 = new myKicks(5, '', 'Clothing', 'https://c0dingforfun.github.io/kic
      'Very comfortable pink hoodie',1, 39.99);
     
     let items = [item1, item2, item3, item4, item5]
-    let main = document.querySelector('main');
+    let products = document.querySelector('.row');
     localStorage.setItem('items', JSON.stringify('items'));
     
     items.forEach(item => {
-        main.innerHTML += `
-                        <div class="row">
-                            <div class="col-lg-4 col-md-4 col-sm-12">
-                                <div class="card products" style="width: 18rem;">
+        products.innerHTML += `
+                                <div class="card" style="width: 18rem;">
                                     <img src="${item.image}" class="card-img-top">
                                 <div class="card-body">
                                     <h5 class="card-title">${item.name}</h5>
                                     <p class="card-text">R${item.price}</p>
                                     <button value="${item.id}">Add To Cart</button>
                                 </div>
-                            </div>
-                        </div>
                      `
     });
     
