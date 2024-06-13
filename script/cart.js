aae760c1a3aee1f75e21c83a4673f8cb77afb9f9
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-13 09:15:36 +0200

    products

--- script/cart.js
+++ script/cart.js
@@ -1,13 +1,15 @@
-// let main = document.querySelector('main');
-// let Kicks = JSON.parse(localStorage.getItem('Kicks'));
-// let purchased = JSON.parse(localStorage.getItem('cart'));
-// let tbody = document.querySelector('tbody')
 
-//     purchased.forEach(item => {
-//         tbody.innerHTML += `<tr>
-//                     <td>${item.image}</td>
-//                     <td>${item.price}</td>
-                    
-//                     </tr>                 
-//                             `
-// });
+// let Kicks = JSON.parse(localStorage.getItem('Kicks'));
+let purchased = JSON.parse(localStorage.getItem('cart'));
+let tbody = document.querySelector('tbody')
+
+    purchased.forEach(item => {
+        tbody.innerHTML += `
+                    <tr>
+                        <td> <img src="${item.image}" ></td>
+                        <td>${item.name}</td>
+                        <td>${item.quantity}</td>
+                        <td>${item.price}</td>
+                    </tr>                 
+                            `
+});
--- script/products.js
+++ script/products.js
@@ -10,49 +10,50 @@ function myKicks(id, brand, name, image, description, quantity, price){
     
 }
 
-const item1 = new myKicks(1, 'Nike', 'Air Force 1', 
+const kicks1 = new myKicks(1, 'Nike', 'Air Force 1', 
     'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(White).png'
     ,'A mash-up of Pegasus past, the Nike P-6000 delivers an early-2000s look with breathable mesh and synthetic overlays. Vibrant colours give it a modern look while its sporty aesthetic features foam cushioning for all-day comfort.', 
     1,29.99);
-const item2 = new myKicks(2, 'Nike', 'KD17 Sunrise', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/KD17(Sunrise).png',
+const kicks2 = new myKicks(2, 'Nike', 'KD17 Sunrise', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/KD17(Sunrise).png',
      'Slim-fit denim jeans for men, made with premium cotton',1, 49.99);
-const item3 = new myKicks(3, 'Nike', 'Electronics', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Max(Yellow&Black).png', 
+const kicks3 = new myKicks(3, 'Nike', 'Air Max', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Max(Yellow&Black).png', 
     'Noise-canceling wireless beats headphones with up to 20 hours of battery life',1, 129.99);
-const item4 = new myKicks(4, '', 'Books', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(Black).png',
+const kicks4 = new myKicks(4, '', 'Books', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(Black).png',
      'The latest bestselling novel by a renowned author',1, 14.99);
-const item5 = new myKicks(5, '', 'Clothing', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(Black).png',
+const kick5 = new myKicks(5, '', 'Clothing', 'https://c0dingforfun.github.io/kicksPics/Images/Nike/Men/Air_Force1(Black).png',
      'Very comfortable pink hoodie',1, 39.99);
     
-    let items = [item1, item2, item3, item4, item5]
+    let kicks = [kicks1, kicks2, kicks3, kicks4, kick5]
     let products = document.querySelector('#products');
-    localStorage.setItem('items', JSON.stringify('items'));
+    localStorage.setItem('items', JSON.stringify('kicks'));
     
-    items.forEach(item => {
+    kicks.forEach(item => {
         products.innerHTML += `
                                 <div class="card" style="width: 18rem;">
                                     <img src="${item.image}" class="card-img-top">
-                                <div class="card-body">
-                                    <h5 class="card-title">${item.name}</h5>
-                                    <p class="card-text">R${item.price}</p>
-                                    <button value="${item.id}">Add To Cart</button>
+                                    <div class="card-body">
+                                        <h5 class="card-title">${item.name}</h5>
+                                        <p class="card-text">R${item.price}</p>
+                                        <button value="${item.id}" class="products">Add To Cart</button>
+                                    </div>    
                                 </div>  
                      `
     });
     
-    // let purcItems = []
+    let purcKicks = []
     
-    // let purcBtns = document.querySelectorAll('.purchase');
+    let purcBtns = document.querySelectorAll('.purchase');
     
-    // function addTocart(id){
-    //     let [item] = items.filter(object => object.id === +id);
-    //     purcItems.push(item);
-    //     localStorage.setItem('cart',JSON.stringify(purcItems));
-    // }
+    function addTocart(id){
+        let [kicks] = kicks.filter(object => object.id === +id);
+        purcKicks.push(kicks);
+        localStorage.setItem('cart',JSON.stringify(purcKicks));
+    }
     
-    // purcBtns.forEach(button =>{
-    //     button.addEventListener('click',(event)=>{
-    //         addTocart(event.target.value);
-    //     })
-    // })
+    purcBtns.forEach(button =>{
+        button.addEventListener('click',(event)=>{
+            addTocart(event.target.value);
+        })
+    })
 
 
