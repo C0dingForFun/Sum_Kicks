4aa5d4f3bbd7f31b5b72152df20dac43a854fc69
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-14 11:29:54 +0200

    products

--- script/cart.js
+++ script/cart.js
@@ -1,11 +1,11 @@
-let purchasedNikes = JSON.parse(localStorage.getItem('purchNike'))
+let purchasedNikes = JSON.parse(localStorage.getItem('nikeKicks'))
 let tbody = document.querySelector('tbody');
 
 
 purchasedNikes.forEach(item => {
     tbody.innerHTML += `
                 <tr>
-                    <td>${item.image}</td>
+                    <td><img src="${item.image}"</td>
                     <td>${item.name}</td>
                     <td>${item.quantity}</td>
                     <td>${item.price}</td>
