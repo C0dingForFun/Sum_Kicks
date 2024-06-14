287a6dc1fbcb5de638b305c37a4112b6245043ce
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-14 11:45:07 +0200

    products

--- script/cart.js
+++ script/cart.js
@@ -4,8 +4,8 @@ let tbody = document.querySelector('tbody');
 
 purchasedNikes.forEach(item => {
     tbody.innerHTML += `
-                <tr>
-                    <td><img src="${item.image}"</td>
+                <tr ">
+                    <td><img src="${item.image}" id="cart"</td>
                     <td>${item.name}</td>
                     <td>${item.quantity}</td>
                     <td>${item.price}</td>
